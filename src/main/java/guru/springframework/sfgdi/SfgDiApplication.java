6cfeb732ccc73016121f6daacf9da13705b68ab4
Damian Grande <damian.grande+1-at-980079093348@alimentiamoci.com>
2021-06-11 17:21:51 +0200

    Dependency Injection using Spring Framework.

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,6 +1,9 @@
 package guru.springframework.sfgdi;
 
+import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
 import guru.springframework.sfgdi.controllers.MyController;
+import guru.springframework.sfgdi.controllers.PropertyInjectedController;
+import guru.springframework.sfgdi.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -12,5 +15,14 @@ public class SfgDiApplication {
         MyController myController = (MyController) cxt.getBean("myController");
         String greeting = myController.sayHello();
         System.out.println(greeting);
+        System.out.println("__________ Property");
+        PropertyInjectedController propertyInjectedController = (PropertyInjectedController) cxt.getBean("propertyInjectedController");
+        System.out.println(propertyInjectedController.getGreeting());
+        SetterInjectedController setterInjectedController = (SetterInjectedController) cxt.getBean("setterInjectedController");
+        System.out.println("__________ Setter");
+        System.out.println(setterInjectedController.getGreeting());
+        System.out.println("__________ Constructor");
+        ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) cxt.getBean("constructorInjectedController");
+        System.out.println(constructorInjectedController.getGreeting());
     }
 }
--- src/main/java/guru/springframework/sfgdi/services/GreetingServiceImp.java
+++ src/main/java/guru/springframework/sfgdi/services/GreetingServiceImp.java
@@ -1,6 +1,9 @@
 package guru.springframework.sfgdi.services;
 
-public class GreetingServiceImp implements GreetingService{
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingServiceImp implements GreetingService {
     @Override
     public String sayGreeting() {
         return "Hello World";
