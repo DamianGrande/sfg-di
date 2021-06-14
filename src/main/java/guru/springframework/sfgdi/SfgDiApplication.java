3dee737f38b105bf6f7377c22a29db633758abc2
Damian Grande <damian.grande+1-at-980079093348@alimentiamoci.com>
2021-06-14 11:21:52 +0200

    Spring Profiles.

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,9 +1,6 @@
 package guru.springframework.sfgdi;
 
-import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
-import guru.springframework.sfgdi.controllers.MyController;
-import guru.springframework.sfgdi.controllers.PropertyInjectedController;
-import guru.springframework.sfgdi.controllers.SetterInjectedController;
+import guru.springframework.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -12,6 +9,8 @@ import org.springframework.context.ApplicationContext;
 public class SfgDiApplication {
     public static void main(String[] args) {
         ApplicationContext cxt = SpringApplication.run(SfgDiApplication.class, args);
+        I18nController i18nController = (I18nController) cxt.getBean("i18nController");
+        System.out.println(i18nController.sayHello());
         MyController myController = (MyController) cxt.getBean("myController");
         System.out.println("__________ Primary Bean");
         System.out.println(myController.sayHello());
