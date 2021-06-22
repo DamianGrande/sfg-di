89ea0db0b40ca8b7a8aaf9bc0f3c44c8290582bd
Damian Grande <damian.grande+1-at-980079093348@alimentiamoci.com>
2021-06-22 17:47:20 +0200

    Properties Binding.

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -4,6 +4,7 @@ import guru.springframework.sfgdi.controllers.*;
 import guru.springframework.sfgdi.datasource.FakeDataSource;
 import guru.springframework.sfgdi.services.PrototypeBean;
 import guru.springframework.sfgdi.services.SingletonBean;
+import guru.springframework.sfgdi.config.SfgConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -38,9 +39,15 @@ public class SfgDiApplication {
         System.out.println(prototypeBean1.getMyScope());
         PrototypeBean prototypeBean2 = cxt.getBean(PrototypeBean.class);
         System.out.println(prototypeBean2.getMyScope());
+        System.out.println("__________ Fake Data Source");
         FakeDataSource fakeDataSource = cxt.getBean(FakeDataSource.class);
         System.out.println(fakeDataSource.getUsername());
         System.out.println(fakeDataSource.getPassword());
         System.out.println(fakeDataSource.getJdbcurl());
+        System.out.println("__________ Config Props Bean");
+        SfgConfiguration sfgConfiguration = cxt.getBean(SfgConfiguration.class);
+        System.out.println(sfgConfiguration.getUsername());
+        System.out.println(sfgConfiguration.getPassword());
+        System.out.println(sfgConfiguration.getJdbcurl());
     }
 }
